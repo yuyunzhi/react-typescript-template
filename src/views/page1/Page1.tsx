e8fd7660685ed15c03d206d75b0f5976dd364325
yuyunzhi <lambert1129@foxmail.com>
2020-12-25 14:44:08 +0800

    feat:图片动态引用

--- src/views/page1/Page1.tsx
+++ src/views/page1/Page1.tsx
@@ -1,15 +1,18 @@
-import React from 'react'
+import React , {useState}from 'react'
 import styles from './Page1.less'
-import Image1 from 'src/assets/img/1.png'
+// import Image1 from 'src/assets/img/1.png'
 import Image2 from 'src/assets/img/2.jpeg'
 import _ from 'lodash'
 function Page1() {
 
+  const [pic1] = useState('2.jpeg')
+  const [pic2] = useState('1.png')
   console.log(_.each);
   return (
       <div className={styles.title}>
         1`23123123
-        <img src={Image1} alt=""/>
+        <img src={require(`src/assets/img/${pic1}`)} alt=""/>
+        <img src={require(`src/assets/img/${pic2}`)} alt=""/>
         <img src={Image2} alt=""/>
       </div>
   )
