4c1938d00caf1f775b5e938a1a2ab102372ccd87
yuyunzhi <lambert1129@foxmail.com>
2020-12-25 14:13:28 +0800

    feat: add async js

--- src/views/index/Index.tsx
+++ src/views/index/Index.tsx
@@ -1,19 +1,24 @@
 import React from 'react'
-import "./index.less"
+import styles from "./index.less"
 import {router} from "../../router/index"
-// import Api from '../../api/index'
 
 function Index() {
-
-
 
   const goTwo = () => {
     router.push('/antv')
   }
 
+  const asyncGetJs = () => {
+      import('./asyncJs').then(res=>{
+        console.log('res async Js',res)
+      })
+  }
+
   return (
-      <div className="App">
+      <div className={styles.app}>
         <div onClick={goTwo}>入口</div>
+
+        <div className={styles.popover} onClick={asyncGetJs}>异步加载</div>
       </div>
   )
 }
--- src/views/page1/Page1.tsx
+++ src/views/page1/Page1.tsx
@@ -2,8 +2,10 @@ import React from 'react'
 import styles from './Page1.less'
 import Image1 from 'src/assets/img/1.png'
 import Image2 from 'src/assets/img/2.jpeg'
+import _ from 'lodash'
 function Page1() {
 
+  console.log(_.each);
   return (
       <div className={styles.title}>
         1`23123123
